ac0382843f90b5e29df710d6551d380cea199369
greg <gregphiggins@gmail.com>
2024-11-27 22:13:55 +0000

    Adds event sink as configurable services. Fixed problem when generating an addEventProcessor with ScheduledTriggerNode

--- src/main/java/com/fluxtion/server/service/scheduler/ScheduledTriggerNode.java
+++ src/main/java/com/fluxtion/server/service/scheduler/ScheduledTriggerNode.java
@@ -5,14 +5,23 @@
 
 package com.fluxtion.server.service.scheduler;
 
+import com.fluxtion.runtime.annotations.builder.AssignToField;
 import com.fluxtion.runtime.annotations.feature.Experimental;
 import com.fluxtion.runtime.annotations.runtime.ServiceRegistered;
 import com.fluxtion.runtime.callback.CallBackNode;
+import com.fluxtion.runtime.callback.InstanceCallbackEvent;
 
 @Experimental
 public class ScheduledTriggerNode extends CallBackNode {
 
     private SchedulerService schedulerService;
+
+    public ScheduledTriggerNode(@AssignToField("event") InstanceCallbackEvent event) {
+        super(event);
+    }
+
+    public ScheduledTriggerNode() {
+    }
 
     @ServiceRegistered
     public void scheduler(SchedulerService schedulerService) {
