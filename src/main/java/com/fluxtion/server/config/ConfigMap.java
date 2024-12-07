05e91e52de5dda5abd2444ba40ad1d2f2cba802a
greg <gregphiggins@gmail.com>
2024-12-07 15:27:16 +0000

    helper method for config access

--- src/main/java/com/fluxtion/server/config/ConfigMap.java
+++ src/main/java/com/fluxtion/server/config/ConfigMap.java
@@ -1,7 +1,6 @@
 /*
  * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
  * SPDX-License-Identifier: AGPL-3.0-only
- *
  */
 
 package com.fluxtion.server.config;
@@ -22,5 +21,10 @@ public class ConfigMap {
     @SuppressWarnings({"raw", "unchecked"})
     public <T> T get(String key) {
         return (T) configMap.get(key);
+    }
+
+    @SuppressWarnings({"raw", "unchecked"})
+    public <T> T getOrDefault(String key, T defaultValue) {
+        return (T) configMap.getOrDefault(key, defaultValue);
     }
 }
