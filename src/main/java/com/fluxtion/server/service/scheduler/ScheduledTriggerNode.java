5c6212c27778f89d196a74c79d15cb14e01a99e4
greg <gregphiggins@gmail.com>
2025-01-19 17:59:52 +0000

    rework ScheduledTriggerNode to be compliant with spring build

--- src/main/java/com/fluxtion/server/service/scheduler/ScheduledTriggerNode.java
+++ src/main/java/com/fluxtion/server/service/scheduler/ScheduledTriggerNode.java
@@ -1,32 +1,30 @@
 /*
- * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
+ * SPDX-FileCopyrightText: © 2025 Gregory Higgins <greg.higgins@v12technology.com>
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 package com.fluxtion.server.service.scheduler;
 
-import com.fluxtion.runtime.annotations.builder.AssignToField;
 import com.fluxtion.runtime.annotations.feature.Experimental;
 import com.fluxtion.runtime.annotations.runtime.ServiceRegistered;
-import com.fluxtion.runtime.callback.CallBackNode;
-import com.fluxtion.runtime.callback.InstanceCallbackEvent;
+import com.fluxtion.runtime.callback.AbstractCallbackNode;
 
 @Experimental
-public class ScheduledTriggerNode extends CallBackNode {
+public class ScheduledTriggerNode extends AbstractCallbackNode<Object> {
 
     private SchedulerService schedulerService;
 
-    public ScheduledTriggerNode(@AssignToField("event") InstanceCallbackEvent event) {
-        super(event);
+    public ScheduledTriggerNode() {
+        super();
     }
 
-    public ScheduledTriggerNode() {
-        this(InstanceCallbackEvent.nextCallBackEvent());
+    public ScheduledTriggerNode(int filterId) {
+        super(filterId);
     }
 
     @ServiceRegistered
-    public void scheduler(SchedulerService schedulerService) {
-        this.schedulerService = schedulerService;
+    public void scheduler(SchedulerService scheduler) {
+        this.schedulerService = scheduler;
     }
 
     public void triggerAfterDelay(long millis) {
